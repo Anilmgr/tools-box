ad465f41175247e9bd3cdf023d886803728aa73b
Anil Baral Magar <mgranil60@gmail.com>
2022-12-11 22:52:31 +0545

    setups

--- src/app/tabs/tabs-routing.module.ts
+++ src/app/tabs/tabs-routing.module.ts
@@ -4,19 +4,19 @@ import { TabsPage } from './tabs.page';
 
 const routes: Routes = [
   {
-    path: 'tabs',
+    path: 'tools',
     component: TabsPage,
     children: [
       {
-        path: 'tab1',
+        path: 'unit-converter',
         loadChildren: () => import('../tab1/tab1.module').then(m => m.Tab1PageModule)
       },
       {
-        path: 'tab2',
+        path: 'calculator-tool',
         loadChildren: () => import('../tab2/tab2.module').then(m => m.Tab2PageModule)
       },
       {
-        path: 'tab3',
+        path: 'exchange-tool',
         loadChildren: () => import('../tab3/tab3.module').then(m => m.Tab3PageModule)
       },
       {
